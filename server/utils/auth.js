4798a041dd129dfc66cc7c8faff269815a136d93
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-21 21:39:21 -0500

    installed apollo server express and updated middleare function to work with GraphQL API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,18 +6,27 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via  req.body, req.query or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
+    if(req.headers.authorization){
+      // split token string into an array and return actual token
       token = token.split(' ').pop().trim();
     }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+    if(!token){
+      return req;
     }
+    
+    // // ["Bearer", "<tokenvalue>"]
+    // if (req.headers.authorization) {
+    //   token = token.split(' ').pop().trim();
+    // }
+
+    // if (!token) {
+    //   return res.status(400).json({ message: 'You have no token!' });
+    // }
 
     // verify token and get user data out of it
     try {
