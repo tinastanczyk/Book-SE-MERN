9e429be10a2c7ed0de6bf6dd51f58a95bb02335c
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-21 23:44:51 -0500

    fixed syntax in mutations file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,8 +27,9 @@ export const ADD_USER = gql`
 export const SAVE_BOOK = gql`
   mutation saveBook($userId: ID!, $bookId: String!) {
     saveBook(userId: $userId, bookId: $bookId) {
-      userId
-      bookId
+      _id
+      username
+      savedBooks
     }
   }
 `;
@@ -37,8 +38,8 @@ export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!){
     removeBook(bookId: $bookId) {
       _id
-      userId
-      bookId
+      username
+      savedBooks
     }
   }
 `;
