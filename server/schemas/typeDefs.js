2efb3eea194ecea7064f7ec8a0da824e3d4d7d5e
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-21 22:08:17 -0500

    created typeDef.js file structure

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(userId: ID!, title: String!, authors: [String]!, description: String!, bookId: ID!. image: String!, link: String!): User
+    removeBook(bookId: ID!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: INT
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]!
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
